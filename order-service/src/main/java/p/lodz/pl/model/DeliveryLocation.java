4509751a7ad8ee96ed546bfa31a96288cbab0f5b
Piotr Pleska <263232@edu.p.lodz.pl>
2026-03-18 20:22:21 +0100

    address fixed

--- order-service/src/main/java/p/lodz/pl/model/DeliveryLocation.java
+++ order-service/src/main/java/p/lodz/pl/model/DeliveryLocation.java
@@ -18,6 +18,15 @@ public class DeliveryLocation extends ControlledEntity {
     @Column(nullable = false, precision = 10, scale = 7)
     public BigDecimal longitude;
 
-    @Column(name = "address_line")
-    public String addressLine;
+    @Column(name = "street_address", nullable = false)
+    public String streetAddress;
+
+    @Column(name = "postal_code", nullable = false, length = 10)
+    public String postalCode;
+
+    @Column(name = "city", nullable = false)
+    public String city;
+
+    @Column(name = "country", nullable = false)
+    public String country = "Polska";
 }
