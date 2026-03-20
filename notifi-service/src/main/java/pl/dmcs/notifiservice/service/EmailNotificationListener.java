dd6b10f6caff5cc53b976b608cd885a0e4d0339b
Adam Madej <263219@edu.p.lodz.pl>
2026-03-20 21:46:13 +0100

    Gateway routing fixed, Notification- service init (email sender + pop up window)
    # Conflicts:
    #	docker-compose.yml
    #	notifi-service/README.md
    #	notifi-service/pom.xml
    #	notifi-service/src/main/java/pl/dmcs/notifiservice/controller/NotificationController.java
    #	notifi-service/src/main/java/pl/dmcs/notifiservice/dto/OrderEvent.java
    #	notifi-service/src/main/java/pl/dmcs/notifiservice/model/NotificationLog.java
    #	notifi-service/src/main/java/pl/dmcs/notifiservice/service/EmailNotificationListener.java
    #	springGateway/pom.xml
    #	springGateway/src/main/resources/application.yml

--- notifi-service/src/main/java/pl/dmcs/notifiservice/service/EmailNotificationListener.java
+++ notifi-service/src/main/java/pl/dmcs/notifiservice/service/EmailNotificationListener.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import pl.dmcs.notifiservice.config.RabbitMQConfig;
 import pl.dmcs.notifiservice.dto.OrderEvent;
 import pl.dmcs.notifiservice.model.NotificationLog;
-import pl.dmcs.notifiservice.model.NotificationStatus;
 import pl.dmcs.notifiservice.repository.NotificationLogRepository;
 
 @Service
@@ -13,44 +12,27 @@ public class EmailNotificationListener {
 
     private final EmailSenderService emailSenderService;
     private final NotificationLogRepository logRepository;
-    private final SseNotificationService sseNotificationService;
 
     public EmailNotificationListener(
             EmailSenderService emailSenderService,
             NotificationLogRepository logRepository,
-            SseNotificationService sseNotificationService) {
         this.emailSenderService = emailSenderService;
         this.logRepository = logRepository;
-        this.sseNotificationService = sseNotificationService;
     }
 
     @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
     public void handleOrderCreatedEvent(OrderEvent event) {
-        NotificationStatus status = NotificationStatus.SUCCESS;
         String errorMessage = null;
 
         try {
             emailSenderService.sendOrderConfirmation(
                     event.customerEmail(),
-                    event.orderId().toString(),
-                    event.status().toString()
             );
 
-            String frontendMessage = "Nowe zamówienie: " + event.orderId().toString() + " (" + event.status() + ")";
-            sseNotificationService.pushNotificationToFrontend(frontendMessage);
 
         } catch (Exception e) {
-            status = NotificationStatus.ERROR;
             errorMessage = e.getMessage();
-            System.err.println("Błąd układu wykonawczego: " + errorMessage);
         } finally {
-            NotificationLog dbLog = NotificationLog.builder()
-                    .orderId(event.orderId())
-                    .recipientEmail(event.customerEmail())
-                    .status(status)
-                    .errorMessage(errorMessage)
-                    .build();
-
             logRepository.save(dbLog);
         }
     }
